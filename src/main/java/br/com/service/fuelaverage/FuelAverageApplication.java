97f04340aa8747191fa8638e2806e39a34a2c050
Silva <monise.candido.silva@accenture.com>
2020-01-10 22:38:33 -0300

    Initial commit

---
+++ src/main/java/br/com/service/fuelaverage/FuelAverageApplication.java
@@ -0,0 +1,13 @@
+package br.com.service.fuelaverage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelAverageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelAverageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/service/fuelaverage/FuelAverageApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.service.fuelaverage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelAverageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
